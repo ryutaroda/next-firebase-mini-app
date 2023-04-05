870427016b4ed03deebb292f0d7b4b780b3455b4
ryutaroda <56154082+ryutaroda@users.noreply.github.com>
2023-04-05 22:27:04 +0900

    Layoutのコンポーネント作成.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../context/auth';
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthProvider>
   );
 }
