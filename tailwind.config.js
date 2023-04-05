21d11f39b47eb3918ea0691769ea052a06316ede
ryutaroda <56154082+ryutaroda@users.noreply.github.com>
2023-04-05 22:42:51 +0900

    webフォント導入.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ["Gloria Hallelujah", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
