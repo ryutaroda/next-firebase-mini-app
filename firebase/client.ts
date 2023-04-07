6b537e8ac2b8ff9fafadde308dca1d8c409983b2
ryutaroda <56154082+ryutaroda@users.noreply.github.com>
2023-04-07 23:20:17 +0900

    認証情報設定.

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,25 +6,33 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MEASUREMENT_ID,
+} from '../constant/env';
 
 const firebaseConfig = {
   // TODO:認証情報を設置
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  projectId: 'xxx',
-  storageBucket: 'xxx',
-  messagingSenderId: 'xxx',
-  appId: 'xxx',
-  measurementId: 'xxx',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 if (!getApps()?.length) {
   // Firebaseアプリの初期化
-  initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 }
 
 // 他ファイルで使うために機能をエクスポート
-const app = initializeApp(firebaseConfig);
 export const analytics =
 process.env.NODE_ENV === "production" && typeof window !== "undefined"
   ? getAnalytics(app)
